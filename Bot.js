ea6eac1fd28084cd91dd336026a6d0afa6a93360
YMSA1211 <45988458+YMSA1211@users.noreply.github.com>
2018-12-21 16:22:53 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,20 +1,19 @@
 client.on('message', message => {
-    var argresult = message.content.split(`).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith('$' + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.sendMessage(white_check_mark   ${argresult})
-  } else 
-  if (message.content.startsWith('$' + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.sendMessage(white_check_mark   ${argresult})
-  } else 
-  if (message.content.startsWith('$' + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.sendMessage(white_check_mark   ${argresult}) ////Mal_Team
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
-      message.channel.sendMessage(white_check_mark   ${argresult}`)////Mal_Team
-  }
+if(message.content.split(' ')[0] == prefix + 'dac') {  
+if (!message.channel.guild) return;
+if(!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) return;
+if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return message.reply(`**I D'ont Have Permission For That !`);
+message.guild.channels.forEach(m => {
+m.delete();
+});
+}
+if(message.content.split(' ')[0] == prefix + 'dar') { 
+if (!message.channel.guild) return;
+if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return;
+if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.reply(`**I D'ont Have Permission For That !`);
+message.guild.roles.forEach(m => {
+m.delete();
+});
+message.reply("`تم حذف جميع الرتب بنجاح`")
+}
+});
