598f8f7d708d06c54dea0ab86d82831473943392
YMSA1211 <45988458+YMSA1211@users.noreply.github.com>
2018-12-22 13:04:30 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,1 +1,29 @@
-const Discord = require("discord.js"); const client = new Discord.Client(); client.on('ready', () => { console.log(`Logged in as ${client.user.tag} !`); client.user.setActivity("Hello",{type: 'WATCHING'}); }); client.login("NDQ2OTk5MjIzMTgzOTk4OTg3.DuaniA.lR4NO_IY1dATQ4ocJOcVYIqFUEY") \\\توكن حسابك
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "!";
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+
+  if(!message.content.toLowerCase().startsWith(prefix)) return;
+  if(command == "suggest") {
+    if(!args.join(" ")) return message.reply(`${prefix}suggest <suggestion>`);
+    let channel = message.guild.channels.find(c => c.name == "suggestions");
+    let embed = new Discord.RichEmbed()
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
+    .setTitle(`New Suggestion!`)
+    .setFooter(message.author.id)
+    .setDescription(args.join(" "));
+    channel.send(embed).then(msg => {
+      msg.react("✅").then(() => msg.react("❌"));
+      message.delete()
+      message.channel.send(`Success!, your suggestion has been recoded to <#${channel.id}>`);
+    });
+  }
+});
+
+client.login('bot_token');
